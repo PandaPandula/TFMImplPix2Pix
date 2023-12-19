6402ce66686245ddd0738d67e765de741c29ae3d
Alexandre Fló <64798753+PandaPandula@users.noreply.github.com>
2023-12-19 19:31:00 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -11,22 +11,20 @@ from tqdm import tqdm
 from PIL import Image
 import numpy as np
 
-#funcion inference para generar imagenes a partir de una imagen de entrada
+
 def inference_image():
-    #cargamos el modelo
     gen = Generator(in_channels=3, features=64).to(config.DEVICE)
     opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999))
     load_checkpoint(
             config.CHECKPOINT_GEN, gen, opt_gen, config.LEARNING_RATE,
     )
     
-    #cargamos la imagen de entrada
     input_image = np.array(Image.open(config.IMAGE_INFERENCE_PATH)) 
     augmentations = config.both_transform(image=input_image)
     input_image = augmentations["image"]
 
     input_image = config.transform_only_input(image=input_image)["image"]
-    # llamamos a la funcion inference
+
     inference(gen, input_image, config.SAVE_INFER)
 
 
